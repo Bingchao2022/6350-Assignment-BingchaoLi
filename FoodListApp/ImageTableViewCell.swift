f947bfedc6f56e75d954576d8684ee4017eaaf24
Bingchao Li <li.bingch@northeastern.edu>
2023-10-02 23:49:24 -0700

    Initial commit

---
+++ FoodListApp/ImageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ImageTableViewCell.swift
+//  FoodListApp
+//
+//  Created by 李炳超 on 10/2/23.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- FoodListApp/ViewController.swift
+++ FoodListApp/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var tblView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell;
+    }
 
 }
 
